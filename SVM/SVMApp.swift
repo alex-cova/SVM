b941f96296ac2d9db82e615cd2cf83763f230346
Alejandro Covarrubias <alex_covag@icloud.com>
2023-07-28 00:08:56 -0600

    Initial Commit

---
+++ SVM/SVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SVMApp.swift
+//  SVM
+//
+//  Created by Alex on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
